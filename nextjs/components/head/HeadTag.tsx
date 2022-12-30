f492c61840da4aa2ba1686ccb02e469a6626cb6c
abdoohossamm <abdoohossamm@outlook.com>
2022-12-30 12:51:44 +0200

    create a HeadTage component

---
+++ nextjs/components/head/HeadTag.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head'
+const HeadTag = () => {
+    return (
+        <Head>
+            <title>ToDo app</title>
+            <meta name="description" content="Abdalrahman Hossam Eldin Mohamed (abdoohossam) portfolio. Fullstack developer (Backend-heavy)." />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    );
+};
+
+export default HeadTag;
